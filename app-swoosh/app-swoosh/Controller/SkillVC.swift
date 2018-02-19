2972098366f9aef5f47eef29de9058b5fc0fcda0
kushwahashsih <kushwah007.as@gmail.com>
2018-02-19 17:15:00 +0530

    adding controller and doing prog segues

---
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -0,0 +1,26 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by Mellifera Labs on 2/19/18.
+//  Copyright © 2018 Mellifera Labs. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
