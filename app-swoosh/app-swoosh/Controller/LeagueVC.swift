20a4eb312c6bb1f32eabdc065405c3e652153a9d
kushwahashsih <kushwah007.as@gmail.com>
2018-02-19 17:43:57 +0530

    model and on click redirect

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,19 +10,39 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
-
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "SkillVC", sender: self)
+    // making identifier name and make swage through controller
     }
+    
     @IBAction func unwindFromSkillVC(LeagueVC: UIStoryboardSegue) {
-        
+        // for reverting back
     }
+  
+    @IBAction func onMensTapped(_ sender: Any) {
+        SelectLeague(leagueType: "Mens")
 
+    }
+    @IBAction func onWomensTappes(_ sender: Any) {
+        SelectLeague(leagueType: "Womens")
+    }
+    
+    @IBAction func onCoidTapped(_ sender: Any) {
+       SelectLeague(leagueType: "coed")
+    }
+    
+    func SelectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
 }
---
+++ app-swoosh/app-swoosh/Model/player.swift
@@ -0,0 +1,16 @@
+//
+//  player.swift
+//  app-swoosh
+//
+//  Created by Mellifera Labs on 2/19/18.
+//  Copyright © 2018 Mellifera Labs. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
